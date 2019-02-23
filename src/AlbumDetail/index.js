7d2cd537bdbcb9e450a59aa79b8cc403ec46dfac
Sahid Hernandez <sahidhr@gmail.com>
2019-02-23 10:10:58 -0800

    time format batter class names

--- src/AlbumDetail/index.js
+++ src/AlbumDetail/index.js
@@ -1,6 +1,11 @@
 import React, { Component } from "react";
 import "./index.scss";
 import Album from "../Album";
+const formatTime = time => {
+  const minutes = Math.floor(time / 60);
+  const seconds = time - minutes * 60;
+  return `${minutes}:${seconds}`;
+};
 const AlbumDetail = ({ album }) => (
   <div className="album-detail">
     <p className="album-detail__track-list__title title">{album.title.value}</p>
@@ -11,40 +16,38 @@ const AlbumDetail = ({ album }) => (
         album={album}
         selectedAlbum={album}
       />
-      <div className="album-detail__track-list__details">
-        <div className="album-detail__track-list__details__table album-detail__track-list__details__table__header">
-          <p className="album-detail__track-list__details__table__number">
-            #
-          </p>
-          <p className="album-detail__track-list__details__element album-detail__track-list__details__table__title">
+      <div className="offset-table">
+        <div className="table__row table__row__headers">
+          <p className="table__row__number">#</p>
+          <p className="offset-table__element table__row__title">
             Title
           </p>
-          <p className="album-detail__track-list__details__element album-detail__track-list__details__table__artist">
+          <p className="offset-table__element table__row__artist">
             Artist
           </p>
-          <p className="album-detail__track-list__details__element album-detail__track-list__details__table__duration">
+          <p className="offset-table__element table__row__duration">
             Time
           </p>
-          <p className="album-detail__track-list__details__element album-detail__track-list__details__table__release-date">
+          <p className="offset-table__element table__row__release-date">
             Released
           </p>
         </div>
         {!album.tracks.loading &&
           album.tracks.value.map((track, i) => (
-            <div className="album-detail__track-list__details__table">
-              <p className="album-detail__track-list__details__table__number">
+            <div className="table__row">
+              <p className="table__row__number">
                 {i + 1}
               </p>
-              <p className="album-detail__track-list__details__element album-detail__track-list__details__table__title">
+              <p className="offset-table__element table__row__title">
                 {track.title.value}
               </p>
-              <p className="album-detail__track-list__details__element album-detail__track-list__details__table__artist">
+              <p className="offset-table__element table__row__artist">
                 {track.artist.value.name}
               </p>
-              <p className="album-detail__track-list__details__element album-detail__track-list__details__table__duration">
-                {track.duration.value}
+              <p className="offset-table__element table__row__duration">
+                {formatTime(track.duration.value)}
               </p>
-              <p className="album-detail__track-list__details__element album-detail__track-list__details__table__release-date">
+              <p className="offset-table__element table__row__release-date">
                 {album.release_date.value}
               </p>
             </div>
