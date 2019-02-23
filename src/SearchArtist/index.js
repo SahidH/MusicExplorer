6eb27436d25e3fc99b13cfbef24326ae51f202bc
Sahid Hernandez <sahidhr@gmail.com>
2019-02-23 11:32:01 -0800

    formatting

--- src/SearchArtist/index.js
+++ src/SearchArtist/index.js
@@ -1,31 +1,29 @@
 import React, { Component } from "react";
 import "./index.scss";
 const SearchArtist = ({ artists, onSearchArtist, onArtistSelected }) => (
-    <div className="search-artist--container">
-      <input
-        className="search-artist--input"
-        placeholder="Search here"
-        onChange={onSearchArtist}
-      />
-      {!!artists.length && (
-        <div className="search-artist--dropdown">
-          <p className="search-artist--dropdown--title">
-            Search Results
-          </p>
-          <div className="search-artist--dropdown--elements">
-            {artists.map(artist => (
-              <div className="search-artist--dropdown--element">
-                <p
-                  className="search-artist--dropdown--element-text"
-                  onClick={() => onArtistSelected(artist)}
-                >
-                  {artist.name}
-                </p>
-              </div>
-            ))}
-          </div>
+  <div className="search-artist--container">
+    <input
+      className="search-artist--input"
+      placeholder="Search here"
+      onChange={onSearchArtist}
+    />
+    {!!artists.length && (
+      <div className="search-artist--dropdown">
+        <p className="search-artist--dropdown--title">Search Results</p>
+        <div className="search-artist--dropdown--elements">
+          {artists.map(artist => (
+            <div
+              className="search-artist--dropdown--element"
+              onClick={() => onArtistSelected(artist)}
+            >
+              <p className="search-artist--dropdown--element-text">
+                {artist.name}
+              </p>
+            </div>
+          ))}
         </div>
-      )}
-    </div>
-  );
+      </div>
+    )}
+  </div>
+);
 export default SearchArtist;
